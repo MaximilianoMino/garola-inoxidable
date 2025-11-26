585d37c2ebea8b38bd4192c9e3c1056bb6a4cc90
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2025-11-26 18:18:14 +0530

    Add Code of Conduct, MIT License, and update README with project details and features

--- src/components/ui/Icons.tsx
+++
@@ -1,8 +0,0 @@
-import { Menu, X, Linkedin, Twitter, Facebook } from 'lucide-react';
-
-export const MenuIcon = Menu;
-export const CloseIcon = X;
-export const LinkedinIcon = Linkedin;
-export const TwitterIcon = Twitter;
-export const FacebookIcon = Facebook;
-
